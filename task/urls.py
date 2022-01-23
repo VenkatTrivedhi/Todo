dcfaa5af0259f2d907b71210ffe727f44fdc1832
Koppisetti Venkatesh <44717853+VenkatTrivedhi@users.noreply.github.com>
2022-01-23 18:23:17 +0530

    Update urls.py

--- task/urls.py
+++ task/urls.py
@@ -4,13 +4,12 @@ from rest_framework.authtoken import views
 from rest_framework import routers
 
 route = routers.DefaultRouter()
-route.register('',UserApi)
+route.register('user',UserApi)
 
 urlpatterns = [
     path("",Create.as_view()),
     path("<int:pk>",Change.as_view()),
     path("api/",include(route.urls)),
     path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),   
-
 ]
 
